462804ee8771fc6de72f2f6c3d4983513ac7ecf7
Cubiwan <cubiwan@gmail.com>
2017-11-29 14:14:11 +0100

    Update Debug.cpp

--- Debug.cpp
+++ Debug.cpp
@@ -5,56 +5,56 @@
 */
 
 #if (ARDUINO >= 100)
-	#include <Arduino.h>
+  #include <Arduino.h>
 #else
-	#include <WProgram.h>
+  #include <WProgram.h>
 #endif
 
 void stop(){
-	Serial.print("STOP!!!");
+  Serial.print("STOP!!!");
   Serial.flush();
-	noInterrupts();
-	while(1){
-		delay(1000);
-	};
+  noInterrupts();
+  while(1){
+    delay(1000);
+  };
 }
 
 void softwareReset() {
-	Serial.print("RESET!!!");
-	Serial.flush();
-	asm volatile ("  jmp 0");
+  Serial.print("RESET!!!");
+  Serial.flush();
+  asm volatile ("  jmp 0");
 }
 
 void assert(bool t){
-	if(!t){
-		Serial.println("Fail");
-		stop();
-	} else {
-		Serial.println("Pass");
-	}
+  if(!t){
+    Serial.println("Fail");
+    stop();
+  } else {
+    Serial.println("Pass");
+  }
 }
 
 void assertNoNull(void* o){
-	if(o == ((void *)0)){
-		Serial.println("Fail");
-		stop();
-	} else {
-		Serial.println("Pass");
-	}
+  if(o == ((void *)0)){
+    Serial.println("Fail");
+    stop();
+  } else {
+    Serial.println("Pass");
+  }
 }
 
 void test(bool t){
-	if(!t){
-		Serial.println("False");
-	} else {
-		Serial.println("True");
-	}
+  if(!t){
+    Serial.println("False");
+  } else {
+    Serial.println("True");
+  }
 }
 
 void testNull(void* o){
-	if(o == ((void *)0)){
-		Serial.println("is null");
-	} else {
-		Serial.println("is no null");
-	}
+  if(o == ((void *)0)){
+    Serial.println("is null");
+   } else {
+    Serial.println("is no null");
+  }
 }
